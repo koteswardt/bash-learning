ffb43ea05a1b6ea9c730da6244c652199cf93aea
koteswardt <koteswardt@gmail.com>
2026-02-06 09:01:03 +1030

    Day 4: Built User Onboarding Automation with Logic + Loops

---
+++ onboard.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# This script automatically onboard new user for instance if new user joins 
+# automatically creates home dir and sub dir and files
+
+# if block to check the username inpute
+#[a-zA-Z]: Ensures there is at least one letter.
+#! [[ ... =~ ^[0-9]+$ ]]: Ensures the input isn't only digits.<S-Del>
+if [[ "$1" =~ [a-zA-Z] ]] && [[ ! "$1" =~ ^[0-9]+$ ]]; then
+    echo "Hi $1 welcome to the team"
+    for folder in scripts config logs
+    do 
+        mkdir -p "$1/$folder"
+    done
+    echo "Welcome $1 your account was created on $(date)." > $1/welcome.txt
+elif [[ -z "$1" ]]; then
+    echo "Sorry Mate your forgot to enter the username"
+else
+    echo "UnserName not valid"
+fi
